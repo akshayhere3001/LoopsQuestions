ea075967540588b8a132a69ce05d32ac573dde7c
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-07-27 11:20:28 +0530

    Create Problem4.java

---
+++ Problem4.java
@@ -0,0 +1,23 @@
+public class Problem4{
+  public static void Main(String[] args){
+      
+      
+      // Problem-5 -> Write a Program to find factorial of given number
+		System.out.println();
+		
+		int factorial = 1; // for Product initialize with 1 
+							// for sum initialize with 0
+		int number = 5;
+		int iter = 1;
+		while(iter <= number) {
+			factorial *= iter;
+			iter++;
+		}
+		
+		System.out.println(factorial);
+		
+      
+      
+  }
+
+}
